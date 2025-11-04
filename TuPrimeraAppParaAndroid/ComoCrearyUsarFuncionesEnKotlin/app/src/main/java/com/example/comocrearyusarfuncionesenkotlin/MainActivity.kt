5558b8175828a6c86b9907f3c4aa8fc8f4ac37f1
AlejandroGuerraJimenez <alejandro.guerra111@alu.ulpgc.es>
2025-11-04 12:59:28 +0000

    Section 'Argumentos con nombre' finished

--- TuPrimeraAppParaAndroid/ComoCrearyUsarFuncionesEnKotlin/app/src/main/java/com/example/comocrearyusarfuncionesenkotlin/MainActivity.kt
+++ TuPrimeraAppParaAndroid/ComoCrearyUsarFuncionesEnKotlin/app/src/main/java/com/example/comocrearyusarfuncionesenkotlin/MainActivity.kt
@@ -8,8 +8,12 @@ fun main() {
     println(greeting)
      */
     //println(birhdayGreeting())
+
     //println(birhdayGreeting("Rover"))
     //println(birhdayGreeting("Rex"))
+
+    println(birhdayGreeting(age = 5, name = "Rover")) // orden incorrecto --> Al nombrar las variables da igual el orden
+    println(birhdayGreeting("Rex", 2))  // orden correcto --> no necesario nombrar las variables
 }
 
 fun birhdayGreeting(): String {
@@ -24,3 +28,10 @@ fun birhdayGreeting(name: String): String {
     val ageGreeting = "You are now 5 years old!"
     return "$nameGreeting\n$ageGreeting"
 }
+
+fun birhdayGreeting(name: String, age: Int): String {
+    val nameGreeting = "Happy Birthday, $name!"
+    val ageGreeting = "You are now $age years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
+
