482f04223de44c0e89abde5c72c414496a00dfd3
AlejandroGuerraJimenez <alejandro.guerra111@alu.ulpgc.es>
2025-11-04 16:16:21 +0000

    Section 'Cambia el tamaño de la fuente' finished

--- TuPrimeraAppParaAndroid/CreaUnDisenoBasico/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ TuPrimeraAppParaAndroid/CreaUnDisenoBasico/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +35,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
     Text(
-        text = message
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp
     )
 }
 
@@ -42,6 +45,8 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Esteban!")
+        GreetingText(
+            message = "Happy Birthday Esteban!"
+        )
     }
 }
