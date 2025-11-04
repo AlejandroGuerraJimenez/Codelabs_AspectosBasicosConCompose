fe2578cc283de25b7d8cc14cc25303288453fc9f
AlejandroGuerraJimenez <alejandro.guerra111@alu.ulpgc.es>
2025-11-04 11:15:49 +0000

    Excercises finished

--- TuPrimeraAppParaAndroid/app/src/main/java/com/example/tuprimerprogramaenkotlin/MainActivity.kt
+++ TuPrimeraAppParaAndroid/app/src/main/java/com/example/tuprimerprogramaenkotlin/MainActivity.kt
@@ -1,9 +1,59 @@
 package com.example.tuprimerprogramaenkotlin
 
-fun imprimirNombre( name : String){
-    println("Hello, $name!")
+fun helloWorld() {
+    println("Hello, world!")
 }
 
-fun main(){
-    imprimirNombre(name = "Alejandro")
+fun imprimirNombre(nombre: String) {
+    print("Hello, $nombre!")
+}
+
+fun imprimirNumeros() {
+    println("1")
+    println("2")
+    println("3")
+}
+
+fun aprendiendoKotlin() {
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+}
+
+fun diasSemana() {
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}
+
+fun pronosticoLluvia() {
+    println("Tomorrow is rainy")
+}
+
+fun pronosticoNieve() {
+    println("There is a chance of snow")
+}
+
+fun imprimirCondicionesClimaticas() {
+    println("Cloudy")
+    println("Partly Cloudy")
+    println("Windy")
+}
+
+fun preguntarClima() {
+    println("How's the weather today?")
+}
+
+fun main() {
+    //helloWorld()
+    //imprimirNombre(nombre = "Alejandro")
+    //imprimirNumeros()
+    //aprendiendoKotlin()
+    //diasSemana()
+    //pronosticoLluvia()
+    //pronosticoNieve()
+    //imprimirCondicionesClimaticas()
+    //preguntarClima()
 }
