b8e8ca1b149bb7a6701b942ee2fe3ff591afcf06
AlejandroGuerraJimenez <alejandro.guerra111@alu.ulpgc.es>
2025-11-04 12:47:33 +0000

    Section 'Define una funcion y llamala' finished

---
+++ TuPrimeraAppParaAndroid/ComoCrearyUsarFuncionesEnKotlin/app/src/main/java/com/example/comocrearyusarfuncionesenkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.comocrearyusarfuncionesenkotlin
+
+
+fun main() {
+    birhdayGreeting()
+}
+
+fun birhdayGreeting() {
+    println("Happy Birthday, Rover!")
+    println("You are now 5 years old!")
+}
