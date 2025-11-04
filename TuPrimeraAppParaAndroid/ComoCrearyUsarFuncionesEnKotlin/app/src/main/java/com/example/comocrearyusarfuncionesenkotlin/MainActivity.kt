1d6be536ac17df652a4ef12f56491cde09fd7e6a
AlejandroGuerraJimenez <alejandro.guerra111@alu.ulpgc.es>
2025-11-04 12:55:02 +0000

    Section 'Funciones con varios parámetros' finished

--- TuPrimeraAppParaAndroid/ComoCrearyUsarFuncionesEnKotlin/app/src/main/java/com/example/comocrearyusarfuncionesenkotlin/MainActivity.kt
+++ TuPrimeraAppParaAndroid/ComoCrearyUsarFuncionesEnKotlin/app/src/main/java/com/example/comocrearyusarfuncionesenkotlin/MainActivity.kt
@@ -7,13 +7,20 @@ fun main() {
     val greeting = birhdayGreeting()
     println(greeting)
      */
-
     //println(birhdayGreeting())
-    
+    //println(birhdayGreeting("Rover"))
+    //println(birhdayGreeting("Rex"))
 }
 
 fun birhdayGreeting(): String {
     val nameGreeting = "Happy Birthday, Rover!"
+    val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
+
+
+fun birhdayGreeting(name: String): String {
+    val nameGreeting = "Happy Birthday, $name!"
     val ageGreeting = "You are now 5 years old!"
     return "$nameGreeting\n$ageGreeting"
 }
